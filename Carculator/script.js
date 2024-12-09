637a0eb2178ac1f4beb0efff779eb181ad8ad77b
usunobun-collab <djmurphy041@gmail.com>
2024-12-09 10:52:12 +0100

    Add files via upload

---
+++ Carculator/script.js
@@ -0,0 +1,16 @@
+// CALCULATOR PROGRAMs
+
+const display = document.getElementById("display");
+
+function appenToDisplay(input){
+    display.value += input;    
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    
+}
